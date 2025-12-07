4b67f962845c1bedce394c281ceba84b9717f4b0
sweta <sweta.b@vit.ac.in>
2025-12-08 00:48:48 +0530

    Ver 1

--- odd_num.py
+++ odd_num.py
@@ -3,17 +3,17 @@ for num in range(1, 101):
         print(num)
 odd_numbers = [n for n in range(1, 101) if n % 2 == 1]
 print(odd_numbers)
-num = int(input("Enter a number: "))
+num = int(input("Enter a number: ")) # number entry
 
 if num % 2 != 0:
     print(num, "is odd")
 else:
     print(num, "is not odd")
-numbers = [10, 15, 22, 33, 47, 50]
+numbers = [10, 15, 22, 33, 47, 50] # list
 
 odd_nums = [n for n in numbers if n % 2 != 0]
 print("Odd numbers:", odd_nums)
 numbers = [10, 15, 22, 33, 47, 50]
 
 odd_nums = [n for n in numbers if n % 2 != 0]
-print("Odd numbers:", odd_nums)
+print("Odd numbers:", odd_nums) # print
